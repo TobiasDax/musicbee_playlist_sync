e7188476a707b2894cf546b03f93f1aeb1a331c9
TobiasDax <tobias@daxserver.de>
2024-02-04 22:03:41 +0100

    added funktion to delete Sync Folder befor script runs and cleaned up some print commands

--- playlist_sync.py
+++ playlist_sync.py
@@ -1,11 +1,19 @@
 import os
-import shutil  # Import shutil for copy2
+import shutil
 
 # Define paths using Docker environment variables
 base_playlist_folder = os.environ.get('BASE_PLAYLIST_FOLDER', '/mnt/media/Music/00_Exported_Playlists/')
 new_playlist_folder = os.environ.get('NEW_PLAYLIST_FOLDER', '/mnt/media/MusicBee/Music_Sync/')
 new_music_folder = os.environ.get('NEW_MUSIC_FOLDER', '/mnt/media/MusicBee/Music_Sync/')
-base_music_source = os.environ.get('BASE_MUSIC_SOURCE', '/mnt/media/Music/')  # Add this line
+base_music_source = os.environ.get('BASE_MUSIC_SOURCE', '/mnt/media/Music/')
+
+# Remove all contents within the folder
+shutil.rmtree(new_music_folder)
+print("Deleted Sync Folder and old Links.")
+
+# Recreate an empty folder
+os.mkdir(new_music_folder)
+print("Recreated Sync Folder.")
 
 # Iterate through all playlists in the base folder and its subfolders
 for root, dirs, files in os.walk(base_playlist_folder):
@@ -14,10 +22,12 @@ for root, dirs, files in os.walk(base_playlist_folder):
             # Construct paths for the current playlist
             base_playlist_path = os.path.join(root, file)
             new_playlist_path = os.path.join(new_playlist_folder, file)
+            print("Constructed Playlist Path")
 
             # Copy the playlist file with metadata preservation
             os.makedirs(os.path.dirname(new_playlist_path), exist_ok=True)
-            shutil.copy2(base_playlist_path, new_playlist_path)  # Use shutil.copy2
+            shutil.copy2(base_playlist_path, new_playlist_path)
+            print("Copy Playlist to new Location.")
 
             with open(base_playlist_path, 'r') as base_playlist_file:
                 # Read lines and process each track
@@ -31,12 +41,13 @@ for root, dirs, files in os.walk(base_playlist_folder):
                     # Construct the new music path using the new music folder
                     new_music_path = os.path.join(new_music_folder, track_path)
 
-                    os.remove(new_music_path)  # Remove existing hard link if present
-
                     # Create new paths if they don't exist
                     os.makedirs(os.path.dirname(new_music_path), exist_ok=True)
+                    print("Created Folder:" new_music_path)
+
 
                     # Create new hard link after potential removal
                     os.link(full_track_path, new_music_path)
+                    print("Created Hardlink:" full_track_path)
 
 print("Playlists copied and tracks hard linked successfully.")
